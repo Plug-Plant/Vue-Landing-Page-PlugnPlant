19694a450dc1c615c3dcc5725a0a69456b0b0120
faqihakih <faqihakih12@gmail.com>
2020-07-13 02:36:41 +0700

    gas

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <header class="text-gray-700 body-font bg-teal-500">
         <nav fixed-top class="flex items-center justify-between flex-wrap bg-green-500 p-6">
           <div class="flex items-center flex-shrink-0 text-white mr-6">
-            <span class="font-semibold text-xl tracking-tight">Plug and Plant</span>
+            <span class="font-semibold text-xl tracking-tight">Plug & Plant</span>
           </div>
           <div class="block lg:hidden">
             <button
@@ -33,7 +33,7 @@
       </header>
 
       <section class=" body-font bg-green-500">
-        <div class="pt-24">
+        <div class="">
           <div class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
             <!--Left Col-->
             <div class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left md:pl-32">
@@ -74,69 +74,54 @@
         </div>
       </section>
 
-      <section class="text-gray-700 body-font">
+      <section class="text-gray-700 body-font" style="margin-top:-60px">
         <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div
             class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">The Problems
+            <h1 class="title-font sm:text-4xl text-3xl mb-2 font-medium">The Problems
             </h1>
-            <p class="mb-8 leading-relaxed">Some poeple get frustrated when they care their plants. <br>
-              they sometime forgot to watering. Don't know what good enviroment for the plant and what kind of things
-              the plants that must avoid.</p>
+            <div class="w-56 mb-4">
+              <div class="h-1 bg-green-500 opacity-25 my-0 py-0 rounded-t"></div>
+            </div>
+            <p class="mb-8 leading-relaxed text-justify">Beberapa orang menjadi frustrasi ketika mereka merawat tanaman mereka. Mereka terkadang lupa untuk menyiram. Tidak tahu lingkungan apa yang baik untuk tanaman dan hal-hal apa yang harus dihindari tanaman.</p>
           </div>
           <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <img class="object-cover object-center rounded" alt="hero" src="../src/assets/img/plant.jpg">
+            <img class="object-cover object-center rounded mx-auto" alt="hero" src="../src/assets/img/plant.jpg" width="60%">
           </div>
         </div>
       </section>
 
 
       <section class="text-gray-700 body-font">
-        <div class="container px-5 py-24 mx-auto flex flex-wrap">
+         <div class="flex flex-col text-center w-full">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font">Features</h1>
+            <div class="w-full mb-4">
+          <div class="h-1 mx-auto bg-green-500 w-40 opacity-25 my-0 py-0 rounded-t"></div>
+        </div>
+          </div>
+        <div class="container px-5 py-20 mx-auto flex flex-wrap">
           <div class="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
-            <img alt="feature" class="object-cover object-center mx-auto" src="@/assets/ss_mobile/2.png" width="320px">
+            <img alt="feature" class="object-cover object-center mx-auto  h-full w-full" src="@/assets/ss_mobile/mobile.png">
           </div>
           <div class="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
             <div class="flex flex-col mb-10 lg:items-start items-center">
               <div
                 class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                  class="w-6 h-6" viewBox="0 0 24 24">
-                  <circle cx="6" cy="6" r="3"></circle>
-                  <circle cx="6" cy="18" r="3"></circle>
-                  <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
-                </svg>
+                <img class="rounded" src="@/assets/img/dashboard.svg" alt="" width="60%">
               </div>
               <div class="flex-grow">
-                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">The Catalyzer</h2>
-                <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan
-                  taxidermy. Gastropub indxgo juice poutine.</p>
-                <a class="mt-3 text-indigo-500 inline-flex items-center">Learn More
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                    <path d="M5 12h14M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
+                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Indikator</h2>
+                <p class="leading-relaxed text-base">Menerima data dari alat yang terhubung baik berupa data Kelembapan tanah, suhu udara, ketersedian air dan vitamin </p>
               </div>
             </div>
             <div class="flex flex-col mb-10 lg:items-start items-center">
               <div
                 class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                  class="w-6 h-6" viewBox="0 0 24 24">
-                  <path d="M15 12h-4l-3 "></path>
-                </svg>
+                <img class="rounded" src="@/assets/img/clock.svg" alt="">
               </div>
               <div class="flex-grow">
                 <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Real-time</h2>
-                <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan
-                  taxidermy. Gastropub indxgo juice poutine.</p>
-                <a class="mt-3 text-indigo-500 inline-flex items-center">Learn More
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                    <path d="M5 12h14M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
+                <p class="leading-relaxed text-base">Memberikan notifikasi kepada penguna secara langsung. Contoh : notifikasi penyiraman air, notifikasi penyiraman vitamin, Dll. notifikasi yang diberikan berdasarkan data yang sesuai kedaaan pada saat itu.</p>
               </div>
             </div>
             <div class="flex flex-col mb-10 lg:items-start items-center">
@@ -149,14 +134,7 @@
               </div>
               <div class="flex-grow">
                 <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Report</h2>
-                <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan
-                  taxidermy. Gastropub indxgo juice poutine.</p>
-                <a class="mt-3 text-indigo-500 inline-flex items-center">Learn More
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                    <path d="M5 12h14M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
+                <p class="leading-relaxed text-base">Pada fitur ini berfungsi memberikan laporan kepada penguna berupa sebuah grafik dalam jangka waktu perhari, perbulan dan pertahun.</p>
               </div>
             </div>
           </div>
@@ -188,76 +166,34 @@
             </g>
           </g>
         </svg>
-        <div class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
-          <div class="flex flex-col w-full md:w-3/5 justify-center items-start text-center md:text-left md:pl-32">
-            <div class="mx-auto flex flex-wrap flex-col md:flex-row items-center">
-              <div class="flex flex-wrap overflow-hidden xl:-mx-3">
-                <div class="w-full font-bold text-xl text-white-800">Contact Information</div>
-                <div class="w-full overflow-hidden md:w-1/2 lg:w-1/2 xl:my-3 xl:w-1/2">
-                  <p class="text-white-600 mb-5 text-sm md:pr-1">Sumbrodo Perumahan Ndalem Parikesit No.B7, Slerok, Kota
-                    Tegal.</p>
-                </div>
-
-                <div class="w-full overflow-hidden md:w-1/2 lg:w-1/2 xl:my-3 xl:w-1/2 md:pl-1">
-                  <p class="text-white-600 text-sm mb-5">
-                    Phone: +62 878-4811-4793
-                    <br />Email: plugintegal@gmail.com
-                  </p>
-                </div>
-              </div>
-              <hr />
-              <div class="overflow-hidden xl:-mx-3">
-                <div class="w-full font-bold text-xl text-white-800">Follow Us</div>
-                <br />
-                <div class="flex flex-wrap overflow-hidden xl:-mx-2">
-                  <div class="w-full overflow-hidden md:w-1/4 lg:w-1/4 xl:my-2 xl:px-2">
-                    <a href class="hover:text-gray hover:border-gray">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24">
-                        <path fill="#FFFFFF"
-                          d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 6c-3.313 0-6 2.686-6 6 0 2.651 1.719 4.9 4.104 5.693.3.056.396-.13.396-.289v-1.117c-1.669.363-2.017-.707-2.017-.707-.272-.693-.666-.878-.666-.878-.544-.373.041-.365.041-.365.603.042.92.619.92.619.535.917 1.403.652 1.746.499.054-.388.209-.652.381-.802-1.333-.152-2.733-.667-2.733-2.965 0-.655.234-1.19.618-1.61-.062-.153-.268-.764.058-1.59 0 0 .504-.161 1.65.615.479-.133.992-.199 1.502-.202.51.002 1.023.069 1.503.202 1.146-.776 1.648-.615 1.648-.615.327.826.121 1.437.06 1.588.385.42.617.955.617 1.61 0 2.305-1.404 2.812-2.74 2.96.216.186.412.551.412 1.111v1.646c0 .16.096.347.4.288 2.383-.793 4.1-3.041 4.1-5.691 0-3.314-2.687-6-6-6z" />
-                      </svg>
-                    </a>
-                  </div>
-
-                  <div class="w-full overflow-hidden md:w-1/4 lg:w-1/4 xl:my-2 xl:px-2">
-                    <a href>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24">
-                        <path fill="#FFFFFF"
-                          d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 7.082c1.602 0 1.792.006 2.425.035 1.627.074 2.385.845 2.46 2.459.028.633.034.822.034 2.424s-.006 1.792-.034 2.424c-.075 1.613-.832 2.386-2.46 2.46-.633.028-.822.035-2.425.035-1.602 0-1.792-.006-2.424-.035-1.63-.075-2.385-.849-2.46-2.46-.028-.632-.035-.822-.035-2.424s.007-1.792.035-2.424c.074-1.615.832-2.386 2.46-2.46.632-.029.822-.034 2.424-.034zm0-1.082c-1.63 0-1.833.007-2.474.037-2.18.1-3.39 1.309-3.49 3.489-.029.641-.036.845-.036 2.474 0 1.63.007 1.834.036 2.474.1 2.179 1.31 3.39 3.49 3.49.641.029.844.036 2.474.036 1.63 0 1.834-.007 2.475-.036 2.176-.1 3.391-1.309 3.489-3.49.029-.64.036-.844.036-2.474 0-1.629-.007-1.833-.036-2.474-.098-2.177-1.309-3.39-3.489-3.489-.641-.03-.845-.037-2.475-.037zm0 2.919c-1.701 0-3.081 1.379-3.081 3.081s1.38 3.081 3.081 3.081 3.081-1.379 3.081-3.081c0-1.701-1.38-3.081-3.081-3.081zm0 5.081c-1.105 0-2-.895-2-2 0-1.104.895-2 2-2 1.104 0 2.001.895 2.001 2s-.897 2-2.001 2zm3.202-5.922c-.397 0-.72.322-.72.72 0 .397.322.72.72.72.398 0 .721-.322.721-.72 0-.398-.322-.72-.721-.72z" />
-                      </svg>
-                    </a>
-                  </div>
-
-                  <div class="w-full overflow-hidden md:w-1/4 lg:w-1/4 xl:my-2 xl:px-2">
-                    <a href>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24">
-                        <path fill="#FFFFFF"
-                          d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2.5 11.5v1.2h1.985c-.08.514-.601 1.51-1.985 1.51-1.194 0-2.17-.99-2.17-2.21s.976-2.21 2.17-2.21c.681 0 1.135.29 1.396.541l.949-.916c-.61-.57-1.401-.915-2.345-.915-1.936 0-3.5 1.565-3.5 3.5s1.564 3.5 3.5 3.5c2.02 0 3.359-1.419 3.359-3.419 0-.23-.024-.405-.055-.58h-3.304zm0 0 8.5 1h-1.5v1.5h-1v-1.5h-1.5v-1h1.5v-1.5h1v1.5h1.5v1z" />
-                      </svg>
-                    </a>
-                  </div>
-
-                  <div class="w-full overflow-hidden md:w-1/4 lg:w-1/4 xl:my-2 xl:px-2">
-                    <a href>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24">
-                        <path fill="#FFFFFF"
-                          d="M16.23 7.102c-2.002-.136-6.462-.135-8.461 0-2.165.148-2.419 1.456-2.436 4.898.017 3.436.27 4.75 2.437 4.898 1.999.135 6.459.136 8.461 0 2.165-.148 2.42-1.457 2.437-4.898-.018-3.436-.271-4.75-2.438-4.898zm-6.23 7.12v-4.444l4.778 2.218-4.778 2.226zm2-12.222c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12z" />
-                      </svg>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="flex flex-wrap flex-col md:flex-row">
-            <div class="mb-4">
-              <label class="block text-gray-700 text-sm font-bold mb-2" for="username">Username</label>
-              <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="username" type="text" placeholder="Username" />
-            </div>
-          </div>
-        </div>
+        <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+    <a class="flex title-font font-medium items-center md:justify-start justify-center text-white-800">
+      <span class="ml-3 text-xl">Plug & Plant</span>
+    </a>
+    <p class="text-xl text-white-800 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2020 PLUGIN
+    </p>
+    <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+      <a class="text-white-800" href="https://www.facebook.com/pluginofficiall" target="_blank">
+        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+        </svg>
+      </a>
+      <a class="ml-3 text-white-800">
+        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+        </svg>
+      </a>
+      <a class="ml-3 text-white-800" href="https://www.instagram.com/pluginofficiall/" target="_blank">
+        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+          <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+          <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+        </svg>
+      </a>
+      <a class="ml-3 text-white-800" href="https://github.com/plugintegal" target="_blank">
+       <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="white" d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
+      </a>
+    </span>
+  </div>
       </footer>
     </div>
   </app>
